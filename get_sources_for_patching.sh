654d19c01bee475fd38cc3a322cd7940ecef075b
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2017-12-08 16:48:27 +0100

    chmod+x for get_sources.sh

--- get_sources_for_patching.sh
+++ get_sources_for_patching.sh
@@ -9,61 +9,48 @@ text=text_cmds-99
 archive=libarchive-54
 curl=curl-105
 
-# Cleanup:
-rm -rf file_cmds shell_cmds text_cmds libutil libinfo libarchive curl
-rm -rf $file $libutil $libinfo $shell $text $archive $curl 
+find . -name .DS_Store -exec rm {} \; -print
 
 # get source for file_cmds
 echo "Getting file_cmds"
 curl https://opensource.apple.com/tarballs/file_cmds/$file.tar.gz -O
 tar xfz $file.tar.gz
 rm $file.tar.gz
-# move to position independent of version number
-# so Xcode project stays valid
-mv $file file_cmds 
-(cd file_cmds ; patch -p1 < ../file_cmds.patch ; cd ..)
-
-# get source for libutil:
-echo "Getting libutil"
-curl https://opensource.apple.com/tarballs/libutil/$libutil.tar.gz -O
-tar xfz $libutil.tar.gz
-rm $libutil.tar.gz
-mv $libutil libutil
-
-# get source for libInfo:
-echo "Getting libinfo"
-curl https://opensource.apple.com/tarballs/Libinfo/$libinfo.tar.gz -O
-tar xfz $libinfo.tar.gz
-rm $libinfo.tar.gz 
-mv $libinfo libinfo
+echo "Creating file_cmds.patch"
+diff -Naur $file file_cmds > file_cmds.patch
+rm -rf $file
 
 # get source for shell_cmds:
 echo "Getting shell_cmds"
 curl https://opensource.apple.com/tarballs/shell_cmds/$shell.tar.gz -O
 tar xfz $shell.tar.gz
 rm $shell.tar.gz
-mv $shell shell_cmds
-(cd shell_cmds ; patch -p1 < ../shell_cmds.patch ; cd ..)
+echo "Creating shell_cmds.patch"
+diff -Naur $shell shell_cmds > shell_cmds.patch
+rm -rf $shell
 
 # get source for text_cmds:
 echo "Getting text_cmds"
 curl https://opensource.apple.com/tarballs/text_cmds/$text.tar.gz -O
 tar xfz $text.tar.gz
 rm $text.tar.gz 
-mv $text text_cmds
-(cd text_cmds ; patch -p1 < ../text_cmds.patch ; cd ..)
+echo "Creating text_cmds.patch"
+diff -Naur $text text_cmds > text_cmds.patch
+rm -rf $text
 
 # get source for BSD-tar: (not gnu-tar because licensing issues).
 curl https://opensource.apple.com/tarballs/libarchive/$archive.tar.gz -O
 tar xfz $archive.tar.gz
 rm $archive.tar.gz
-mv $archive libarchive
-(cd libarchive ; patch -p1 < ../libarchive.patch ; cd ..)
+echo "Creating libarchive.patch"
+diff -Naur $archive libarchive > libarchive.patch
+rm -rf $archive
 
 # get source for curl. This one requires OpenSSH + libssl
 curl https://opensource.apple.com/tarballs/curl/$curl.tar.gz -O
 tar xfz $curl.tar.gz
 rm $curl.tar.gz
-mv $curl curl
-(cd curl ; patch -p1 < ../curl.patch ; cd ..)
+echo "Creating curl.patch"
+diff -Naur $curl curl > curl.patch
+rm -rf $curl
 
