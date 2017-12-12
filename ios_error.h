7cad6965f3262e6626b50a64692c4856d8efd2c0
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2017-12-13 00:30:23 +0100

    For stability: removed all calls to err/errx/warn/warnx

--- ios_error.h
+++ ios_error.h
@@ -12,42 +12,10 @@
 #include <stdarg.h>
 #include <pthread.h>
 
-#ifdef _ERR_H_ // did we go through system err.h?
-static void myerrx(int i, const char * fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vwarnx(fmt, ap);
-    va_end(ap);
-    pthread_exit(NULL);
-}
-
-static void myerr(int i, const char * fmt, ...) {
-    va_list ap;
-    va_start(ap, fmt);
-    vwarn(fmt, ap);
-    va_end(ap);
-    pthread_exit(NULL);
-}
-
-// Not working yet.
-static void signal_catcher(int sig) {
-    if ((sig == SIGINT) || (sig == SIGQUIT) || (sig == SIGKILL) || (sig == SIGABRT) || (sig == SIGSTOP) || (sig == SIGHUP)) {
-        (void)signal(sig, SIG_DFL); // stop using this signal catcher
-        pthread_exit(NULL);
-    }
-}
-
-static void mywarn(const char * fmt, ...) {
-    if (stderr == 0) return; // avoid crashing the entire app
-    va_list ap;
-    va_start(ap, fmt);
-    vwarn(fmt, ap);
-    va_end(ap);
-}
-#define errx myerrx
-#define err myerr
-#define warn mywarn
-#endif // _ERR_H_
+#define errx compileError
+#define err compileError
+#define warn compileError
+#define warnx compileError
 
 #define exit(a) pthread_exit(NULL)
 #define _exit(a) pthread_exit(NULL)
