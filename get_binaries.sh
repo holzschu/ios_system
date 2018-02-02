7956e2a98e2ea2c64b69487f80747f89b654722d
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2018-02-02 13:09:49 +0100

    Bug fix in get_binaries.sh

--- get_binaries.sh
+++ get_binaries.sh
@@ -7,8 +7,8 @@ HHROOT="https://github.com/holzschu"
 (cd "${BASH_SOURCE%/*}/Frameworks"
 # ios_system
 echo "Downloading ios_system.framework.zip"
-curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/ios_system.framework.tar.gz
-( tar -xzf ios_system.framework.tar.gz && rm ios_system.framework.tar.gz && mv release/* . ) || { echo "ios_system failed to download"; exit 1; }
+curl -OL $HHROOT/ios_system/releases/download/v$IOS_SYSTEM_VER/release.tar.gz
+( tar -xzf release.tar.gz && rm release.tar.gz && mv release/* . ) || { echo "ios_system failed to download"; exit 1; }
 )
 
 
