7884801ccde9017d244982720cc18197c02ddf9e
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2019-09-12 20:49:22 +0200

    Fixed a bug in fix-frameworks-archs.sh

--- fix-framework-archs.sh
+++ fix-framework-archs.sh
@@ -3,8 +3,8 @@
 lipo -remove i386 release/openssl.framework/openssl -o release/openssl.framework/openssl
 lipo -remove x86_64 release/openssl.framework/openssl -o release/openssl.framework/openssl
 
-lipo -remove i386 release/libssh2.framework/libssh2 -o release/libssh2.framework/libssh2
-lipo -remove x86_64 release/libssh2.framework/libssh2 -o release/libssh2.framework/libssh2
+lipo -remove i386 smallRelease/openssl.framework/openssl -o smallRelease/openssl.framework/openssl
+ipo -remove x86_64 release/libssh2.framework/libssh2 -o release/libssh2.framework/libssh2
 
 lipo -remove i386 smallRelease/openssl.framework/openssl -o smallRelease/openssl.framework/openssl
 lipo -remove x86_64 smallRelease/openssl.framework/openssl -o smallRelease/openssl.framework/openssl
