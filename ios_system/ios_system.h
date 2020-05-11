8a8ada851de6c33e721eb46a2e298e1736e6de04
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-05-11 13:03:41 +0200

    Added ios_getenv for webAssembly

--- ios_system/ios_system.h
+++ ios_system/ios_system.h
@@ -39,6 +39,7 @@ extern void ios_releaseThreadId(pid_t pid);
 extern pid_t ios_currentPid(void);
 extern int ios_getCommandStatus(void);
 extern const char* ios_progname(void);
+extern char * ios_getenv(const char *name);
 extern pid_t ios_fork(void);
 extern void ios_waitpid(pid_t pid);
 extern void ios_signal(int signal); 
