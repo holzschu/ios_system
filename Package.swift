fcc10a8925774a9ebaee0c6050ceec2c59659841
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-06 17:11:31 +0200

    Dynamic library

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "ios_system",
+            type: .dynamic, 
             targets: ["ios_system"]),
     ],
     dependencies: [
