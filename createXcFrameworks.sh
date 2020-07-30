5b86defbbe2d4ad2d15c81cb4ee325d304d7513b
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2020-07-30 15:58:19 +0200

    Fixed an issue with Xcode 12b3: need to specify -arch x86_64 for iphonesimulator

--- createXcFrameworks.sh
+++ createXcFrameworks.sh
@@ -1,6 +1,6 @@
 # First, creat all frameworks for both architectures: 
 xcodebuild -project ios_system.xcodeproj -alltargets -sdk iphoneos -configuration Release -quiet
-xcodebuild -project ios_system.xcodeproj -alltargets -sdk iphonesimulator -configuration Release -quiet
+xcodebuild -project ios_system.xcodeproj -alltargets -sdk iphonesimulator  -arch x86_64 -configuration Release -quiet
 
 # then, merge them into XCframeworks:
 for framework in awk curl_ios files ios_system shell ssh_cmd tar text
