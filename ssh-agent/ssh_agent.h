f639b5f1a05b64b945e2d707a1223b28fe270e7f
HOLZSCHUCH Nicolas <nicolas.holzschuch@inria.fr>
2024-03-22 18:26:01 +0100

    Added ssh-agent and multiple ssh commands

---
+++ ssh-agent/ssh_agent.h
@@ -0,0 +1,19 @@
+//
+//  ssh_agent.h
+//  ssh-agent
+//
+//  Created by Nicolas Holzschuch on 08/02/2024.
+//  Copyright © 2024 Nicolas Holzschuch. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ssh_agent.
+FOUNDATION_EXPORT double ssh_agentVersionNumber;
+
+//! Project version string for ssh_agent.
+FOUNDATION_EXPORT const unsigned char ssh_agentVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ssh_agent/PublicHeader.h>
+
+
